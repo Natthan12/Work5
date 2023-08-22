3cb21f9d885d96360a4cbfdf533ae8b887578d57
Natthan12 <264318241002-st@rmutsb.ac.th>
2023-08-22 11:55:59 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+//import ColumnItem from './column-item';//
+import LayOut from './layout';
+import LayOut2 from './layout2';
+import Question from './Question';
+
+export default function App() {
+  return (
+      //<ColumnItem/>//
+      //<RowItem/>//
+     <Question/>
+      
+  );
+}
